5fd329cba0abf0b549aed1cc1025da7bb4a68fc0
TruongTriKhong <114063384+TruongTriKhong@users.noreply.github.com>
2023-09-18 12:27:32 +0700

    Add files via upload

---
+++ script/word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "flow-chart",
+        hint: "a diagram of an algorithm with arrows, like pseudocode but with drawings, which shows visually how something will work"
+    },
+    {
+        word: "ethics",
+        hint: "deciding what is morally right and wrong in computing -- and in life"
+    },
+    {
+        word: "media",
+        hint: "mobiles, ipads, laptops, television, newspapers, facebook, instagram..."
+    },
+    {
+        word: "privacy",
+        hint: "keeping yourself and your personal information data safe when off and online"
+    },
+    {
+        word: "teamwork",
+        hint: "working together cooperatively, pulling together, turning a group into a team"
+    },
+    {
+        word: "pairs",
+        hint: "working in pairs to learn and practice programming is a practical way to get through that initial slog of learning to work on problem solving and programming"
+    },
+    {
+        word: "perseverance",
+        hint: "sticking at it, hanging in there; important for programming -- and for living"
+    },
+    {
+        word: "resilience",
+        hint: "bouncing back, (from failure), important for programming especially when dealing with mistakes in programming -- and for living"
+    },
+    {
+        word: "resource",
+        hint: "in computing try Computing at School website"
+    },
+    {
+        word: "sequence",
+        hint: "one of the 5 control structures in programming, which illustrates the importance of the order in which a set of operations is performed"
+    }
+]
